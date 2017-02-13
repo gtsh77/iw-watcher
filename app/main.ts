d543053fb6b09e0f260358caef643728f0a6ae6e
Anton Makridin <a.i.makridin@yandex.ru>
2017-02-13 00:27:46 -0500

    crypto DSA hash gen added, playerAuth feat added, inserting players into db

--- app/main.ts
+++ app/main.ts
@@ -13,29 +13,11 @@ class Main {
 		password        : 'test',
 		database		: 'iwstats'		
 	});
+	protected crypto = require('crypto');
 	public init(): void {
 		this.dgram.bind(77,'192.168.0.19');
 		this.dgram.on('listening',() => console.log(`Listening to ${this.dgram.address().address} on ${this.dgram.address().port}`));
 		this.dgram.on('message', msg => this.msgHandler(msg));
-	}
-	public attackHandler(msg: any): void {
-		let arMsg: string[] = msg.match(/(?:L )(\d{2})\/(\d{2})\/(\d{4})(?: - )(\d{2}):(\d{2}):(\d{2})(?:: ")(.[^<\d]+)(?:.+)(STEAM_[\d|:]+|BOT)(?:.+)(connected|entered|disconnected|switched|triggered|attacked|killed)(?: ")(.[^<\d]+)(?:.+)(STEAM_[\d|:]+|BOT)(?:.+with ")(\w+)(?:".+damage ")(\w+)(?:".+health ")(\w+)"/),
-			d = arMsg[1],
-			m = arMsg[2],
-			y = arMsg[3],
-			h = arMsg[4],
-			mi = arMsg[5],
-			se = arMsg[6],
-			nick = arMsg[7],
-			playerSteam = arMsg[8],
-			playerAction = arMsg[9],
-			victimNick = arMsg[10],
-			victimSteam = arMsg[11],
-			weapon = arMsg[12],
-			damage = arMsg[13],
-			rHp = arMsg[14];
-
-			console.log(`## ${nick} - ${playerSteam} ${playerAction} ${victimNick} - ${victimSteam} w ${weapon} w ${damage} rHp ${rHp}`);		
 	}
 	public msgHandler(msg: any): void {
 		try {
@@ -47,58 +29,85 @@ class Main {
 				h = arMsg[4],
 				mi = arMsg[5],
 				se = arMsg[6],
-				nick = arMsg[7],
-				playerSteam = arMsg[8],
+				playerNickName = arMsg[7],
+				playerSteamId = arMsg[8],
 				playerAction = arMsg[9];
 
 			if(playerAction === 'attacked') this.attackHandler(utMsg);
-			else console.log(`## ${nick} - ${playerSteam} ${playerAction}`);				
+			else if(playerAction === 'connected') this.authHandler(playerSteamId,playerNickName);
+			else console.log(`## ${playerNickName} - ${playerSteamId} ${playerAction}`);		
 		}
 		catch(e){
 			console.log(`not_parsed: ${msg.toString('utf8')}`);
 		}
-
-
 		//let stMsg: string = msg.toString('utf8').slice(5,-1);
+	}
+	public attackHandler(stMsg: string): void {
+		let arMsg: string[] = stMsg.match(/(?:L )(\d{2})\/(\d{2})\/(\d{4})(?: - )(\d{2}):(\d{2}):(\d{2})(?:: ")(.[^<\d]+)(?:.+)(STEAM_[\d|:]+|BOT)(?:.+)(connected|entered|disconnected|switched|triggered|attacked|killed)(?: ")(.[^<\d]+)(?:.+)(STEAM_[\d|:]+|BOT)(?:.+with ")(\w+)(?:".+damage ")(\w+)(?:".+health ")(\w+)"/),
+			d = arMsg[1],
+			m = arMsg[2],
+			y = arMsg[3],
+			h = arMsg[4],
+			mi = arMsg[5],
+			se = arMsg[6],
+			playerNickName = arMsg[7],
+			playerSteamId = arMsg[8],
+			playerAction = arMsg[9],
+			victimNickName = arMsg[10],
+			victimSteamId = arMsg[11],
+			weapon = arMsg[12],
+			damage = arMsg[13],
+			rHp = arMsg[14];
 
-//*** сказать
-//L 02/12/2017 - 19:15:30: "Shaman<2><STEAM_1:0:9977876><TERRORIST>" say "rank"
+			console.log(`## ${playerNickName} - ${playerSteamId} ${playerAction} ${victimNickName} - ${victimSteamId} w ${weapon} w ${damage} rHp ${rHp}`);		
+	}
+	public authHandler(steamId: string, playerNickName: string): void {
+		if(steamId === 'BOT') steamId = `BOT_${playerNickName}`;
+		//обновим запись в базе или создадим новую
+		this.pool.getConnection((err, connection) => {
+			if(err) console.log(err);
+			connection.query(`INSERT INTO players (steamId,hash) VALUES('${steamId}','${this.crypto.createHash('DSA').update(steamId+'_iwstats').digest('hex').slice(0,6)}') ON DUPLICATE KEY UPDATE steamId = steamId`,(err, rows, fields) => {
+				if(err) console.log(err);
+			});
+			connection.release();
+		});
+		//обновим историю никнеймов
+		//...
+		//...
+	}
 
-//*** атака
-//L 02/12/2017 - 19:14:16: "Shaman<2><STEAM_1:0:9977876><TERRORIST>" [224 2229 -127] attacked "Paul<23><BOT><CT>" [334 2434 -127] with "ak47" (damage "27") (damage_armor "4") (health "12") (armor "84") (hitgroup "chest")
+	static msgList(): void{
+		//*** сказать
+		//L 02/12/2017 - 19:15:30: "Shaman<2><STEAM_1:0:9977876><TERRORIST>" say "rank"
 
-//L 02/12/2017 - 19:14:16: "Shaman<2><STEAM_1:0:9977876><TERRORIST>" [224 2229 -127] attacked "Paul<23><BOT><CT>" [334 2434 -127] with "ak47" (damage "27") (damage_armor "4") (health "0") (armor "79") (hitgroup "chest")
+		//*** атака
+		//L 02/12/2017 - 19:14:16: "Shaman<2><STEAM_1:0:9977876><TERRORIST>" [224 2229 -127] attacked "Paul<23><BOT><CT>" [334 2434 -127] with "ak47" (damage "27") (damage_armor "4") (health "12") (armor "84") (hitgroup "chest")
 
-//*** фраг
-//L 02/12/2017 - 19:14:17: "Shaman<2><STEAM_1:0:9977876><TERRORIST>" [224 2229 -127] killed "Paul<23><BOT><CT>" [334 2434 -63] with "ak47"
+		//L 02/12/2017 - 19:14:16: "Shaman<2><STEAM_1:0:9977876><TERRORIST>" [224 2229 -127] attacked "Paul<23><BOT><CT>" [334 2434 -127] with "ak47" (damage "27") (damage_armor "4") (health "0") (armor "79") (hitgroup "chest")
 
-//*** новый раунд
-//msg L 02/12/2017 - 19:18:38: World triggered "Round_Start"
+		//*** фраг
+		//L 02/12/2017 - 19:14:17: "Shaman<2><STEAM_1:0:9977876><TERRORIST>" [224 2229 -127] killed "Paul<23><BOT><CT>" [334 2434 -63] with "ak47"
 
-//*** флоу коннекта
-// msg L 02/12/2017 - 19:21:20: "Shaman<25><STEAM_1:0:9977876><>" connected, address ""
+		//*** новый раунд
+		//msg L 02/12/2017 - 19:18:38: World triggered "Round_Start"
 
-// msg L 02/12/2017 - 19:21:21: "Shaman<25><STEAM_1:0:9977876><>" STEAM USERID validated
+		//*** флоу коннекта
+		// msg L 02/12/2017 - 19:21:20: "Shaman<25><STEAM_1:0:9977876><>" connected, address ""
 
-// msg L 02/12/2017 - 19:21:24: "Shaman<25><STEAM_1:0:9977876><>" entered the game
+		// msg L 02/12/2017 - 19:21:21: "Shaman<25><STEAM_1:0:9977876><>" STEAM USERID validated
 
-// msg L 02/12/2017 - 19:21:25: "Shaman<25><STEAM_1:0:9977876><Unassigned>" triggered "clantag" (value "")
+		// msg L 02/12/2017 - 19:21:24: "Shaman<25><STEAM_1:0:9977876><>" entered the game
 
-// msg L 02/12/2017 - 19:21:32: "Shaman<25><STEAM_1:0:9977876>" switched from team <Unassigned> to <TERRORIST>
+		// msg L 02/12/2017 - 19:21:25: "Shaman<25><STEAM_1:0:9977876><Unassigned>" triggered "clantag" (value "")
 
-//*** флоу дисконнекта
-// msg L 02/12/2017 - 19:22:58: "Shaman<25><STEAM_1:0:9977876><TERRORIST>" disconnected (reason "Disconnect")
+		// msg L 02/12/2017 - 19:21:32: "Shaman<25><STEAM_1:0:9977876>" switched from team <Unassigned> to <TERRORIST>
 
-// msg L 02/12/2017 - 19:22:58: "Shaman<25><STEAM_1:0:9977876><TERRORIST>" triggered "Dropped_The_Bomb"
+		//*** флоу дисконнекта
+		// msg L 02/12/2017 - 19:22:58: "Shaman<25><STEAM_1:0:9977876><TERRORIST>" disconnected (reason "Disconnect")
 
-// msg L 02/12/2017 - 19:22:58: "Shaman<25><STEAM_1:0:9977876>" switched from team <TERRORIST> to <Unassigned>
+		// msg L 02/12/2017 - 19:22:58: "Shaman<25><STEAM_1:0:9977876><TERRORIST>" triggered "Dropped_The_Bomb"
 
-		//разбор строки через регулярку
-		
-
-		//определенное действие - метод
-
-		//отправка ответа на сервер
+		// msg L 02/12/2017 - 19:22:58: "Shaman<25><STEAM_1:0:9977876>" switched from team <TERRORIST> to <Unassigned>		
 	}
 
 }
