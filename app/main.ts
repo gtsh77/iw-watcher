f0c2c5f952b275bf7e00d564aa0cb8cc38451c8e
Anton Makridin <a.i.makridin@yandex.ru>
2017-02-15 22:10:36 +0300

    registration, dates added

--- app/main.ts
+++ app/main.ts
@@ -14,6 +14,21 @@ class Main {
 		database		: 'iwstats'		
 	});
 	protected crypto = require('crypto');
+	public readISO(ISOLocal): Date {
+		return new Date(ISOLocal+'+03:00');
+	}
+	public buildISO(date): string {
+		if(!date || typeof date !== 'object') return null;
+		var str = '';
+		date = new Date(date.getTime() + (date.getTimezoneOffset() * 60000) + 10800000);			
+		str += date.getFullYear() + '-';
+		str += ((date.getMonth() < 10)?'0'+(date.getMonth() + 1):(date.getMonth() + 1)) + '-';
+		str += ((date.getDate() < 10)?'0'+date.getDate():date.getDate()) + 'T';
+		str += ((date.getHours() < 10)?'0'+date.getHours():date.getHours()) + ':';
+		str += ((date.getMinutes() < 10)?'0'+date.getMinutes():date.getMinutes()) + ':';
+		str += ((date.getSeconds() < 10)?'0'+date.getSeconds():date.getSeconds());
+		return str;
+	}
 	public init(): void {
 		this.dgram.bind(77,'192.168.0.19');
 		this.dgram.on('listening',() => console.log(`Listening to ${this.dgram.address().address} on ${this.dgram.address().port}`));
@@ -23,19 +38,20 @@ class Main {
 		try {
 			let utMsg = msg.toString('utf8'),
 				arMsg: string[] = utMsg.match(/L (\d{2})\/(\d{2})\/(\d{4}) - (\d{2}):(\d{2}):(\d{2}): (.[^<\d]+).+(STEAM_[\d|:]+|BOT).+(connected|entered|disconnected|switched|triggered|attacked|killed)/),
-				d = arMsg[1],
-				m = arMsg[2],
+				m = arMsg[1],
+				d = arMsg[2],
 				y = arMsg[3],
 				h = arMsg[4],
 				mi = arMsg[5],
 				se = arMsg[6],
 				playerNickName = arMsg[7],
 				playerSteamId = arMsg[8],
-				playerAction = arMsg[9];
+				playerAction = arMsg[9],
+				date = new Date(+y,+m-1,+d,+h,+mi,+se);
 
 			if(playerAction === 'attacked') this.attackHandler(utMsg);
-			else if(playerAction === 'connected') this.authHandler(playerSteamId,playerNickName);
-			else console.log(`## ${playerNickName} - ${playerSteamId} ${playerAction}`);		
+			else if(playerAction === 'connected') this.authHandler(playerSteamId,playerNickName,date);
+			else console.log(`## ${date.toLocaleTimeString()} ${playerNickName} - ${playerSteamId} ${playerAction}`);
 		}
 		catch(e){
 			console.log(`not_parsed: ${msg.toString('utf8')}`);
@@ -44,8 +60,8 @@ class Main {
 	}
 	public attackHandler(stMsg: string): void {
 		let arMsg: string[] = stMsg.match(/(?:L )(\d{2})\/(\d{2})\/(\d{4})(?: - )(\d{2}):(\d{2}):(\d{2})(?:: ")(.[^<\d]+)(?:.+)(STEAM_[\d|:]+|BOT)(?:.+)(connected|entered|disconnected|switched|triggered|attacked|killed)(?: ")(.[^<\d]+)(?:.+)(STEAM_[\d|:]+|BOT)(?:.+with ")(\w+)(?:".+damage ")(\w+)(?:".+health ")(\w+)"/),
-			d = arMsg[1],
-			m = arMsg[2],
+			m = arMsg[1],
+			d = arMsg[2],
 			y = arMsg[3],
 			h = arMsg[4],
 			mi = arMsg[5],
@@ -57,20 +73,49 @@ class Main {
 			victimSteamId = arMsg[11],
 			weapon = arMsg[12],
 			damage = arMsg[13],
-			rHp = arMsg[14];
+			rHp = arMsg[14],
+			date = new Date(+y,+m-1,+d,+h,+mi,+se);
 
-			console.log(`## ${playerNickName} - ${playerSteamId} ${playerAction} ${victimNickName} - ${victimSteamId} w ${weapon} w ${damage} rHp ${rHp}`);
+			console.log(`## ${date.toLocaleTimeString()} ${playerNickName} - ${playerSteamId} ${playerAction} ${victimNickName} - ${victimSteamId} w ${weapon} w ${damage} rHp ${rHp}`);
 	}
-	public authHandler(steamId: string, playerNickName: string): void {
-		if(steamId === 'BOT') steamId = `BOT_${playerNickName}`;
-		//обновим запись в базе или создадим новую
+	public registerNewPlayer(steamId: string, playerNickName: string, date: Date): void {
 		this.pool.getConnection((err, connection) => {
 			if(err) console.log(err);
-			connection.query(`INSERT INTO players (steamId,hash) SELECT '${steamId}','${this.crypto.createHash('DSA').update(steamId+'_iwstats').digest('hex').slice(0,6)}' from DUAL where (SELECT COUNT(*) from players where steamId = '${steamId}') < 1`,(err, rows, fields) => {
+			connection.query(`
+				INSERT INTO players (steamId,hash, createdAt) SELECT '${steamId}','${this.crypto.createHash('DSA').update(steamId+'_iwstats').digest('hex').slice(0,6)}', '${this.buildISO(date)}' from DUAL where (SELECT COUNT(*) from players where steamId = '${steamId}') < 1
+				`,(err, rows, fields) => {
 				if(err) console.log(err);
+				connection.query(`
+					INSERT INTO profiles (nickName, playerId) SELECT '${playerNickName}',id FROM players WHERE steamId = '${steamId}';
+					`,(err, rows, fields) => {
+					if(err) console.log(err);
+						connection.release();
+					});
+			});
+			
+		});		
+	}
+	public authHandler(steamId: string, playerNickName: string, date: Date): void {
+		if(steamId === 'BOT') steamId = `BOT_${playerNickName}`;
+
+		//проверим есть ли игрок в базе
+		this.pool.getConnection((err, connection) => {
+			if(err) console.log(err);
+			connection.query(`SELECT hash from players where steamId = '${steamId}'`,(err, rows, fields) => {
+				if(err) console.log(err);
+				else if(rows.length) console.log(rows[0].hash);
+				else this.registerNewPlayer(steamId, playerNickName, date);
 			});
 			connection.release();
 		});
+		//обновим запись в базе или создадим новую
+		// this.pool.getConnection((err, connection) => {
+		// 	if(err) console.log(err);
+		// 	connection.query(`INSERT INTO players (steamId,hash) SELECT '${steamId}','${this.crypto.createHash('DSA').update(steamId+'_iwstats').digest('hex').slice(0,6)}' from DUAL where (SELECT COUNT(*) from players where steamId = '${steamId}') < 1`,(err, rows, fields) => {
+		// 		if(err) console.log(err);
+		// 	});
+		// 	connection.release();
+		// });
 		//обновим историю никнеймов
 		//...
 		//...
